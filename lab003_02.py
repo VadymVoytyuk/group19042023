c411696905e37534b834709b326ff6b11c140b8a
Vadim Voytyuk <vadimvoytyuk@gmail.com>
2023-04-29 23:13:00 +0300

    finish

---
+++ lab003_02.py
@@ -0,0 +1,24 @@
+name = input('please enter your name >>>>').title().lstrip(' 0123456789')
+surname = input('please enter your surname >>>>').upper().lstrip(' 0123456789')
+#print(name , surname)
+name_length = len(name)
+template = 'Hello,{name} {surname},did you know ,that your name contains {name_length} letters'.strip('')
+name_info = template.format(name=name, surname=surname, name_length=name_length)
+#print(name_info)
+name_info_updated = name_info.replace('Hello', 'Hi',)
+#print(name_info_updated)
+name_info_final= f'{name_info_updated} ?'
+print(name_info_final)
+
+
+
+
+
+
+# Привіт, Андрій ШЕВЧЕНКО, а ти знав, що твоє імя складється з 6 літер"
+# city = 'Kyiv'
+# country = 'Ukraine'
+# continent = 'Europe'
+# template = '{city} -  {country} {continent}'
+# kyiv_info = template.format(city=city, country=country, continent=continent)
+# print(kyiv_info)
