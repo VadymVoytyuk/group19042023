4fd7a38b504d51f66ba4df01b1a2f9563594a4e7
Vadim Voytyuk <vadimvoytyuk@gmail.com>
2023-05-02 08:24:50 +0300

    numbers

---
+++ numbers.py
@@ -0,0 +1,58 @@
+#from decimal import Decimal
+import math
+
+
+# first_number = int(input('Enter your number >>>> 1'))
+# second_number = int(input('Enter your number >>>> 2'))
+
+#MSG_INPUT_NUMBER = 'Enter your number {position} (allowed numbers are 5  5.432 >>>)'
+
+# first_number = float(input(MSG_INPUT_NUMBER.format(position=1)))
+# second_number = float(input(MSG_INPUT_NUMBER.format(position=2)))
+# first_number = 40
+# second_number = 0.02
+
+# summa = first_number + second_number
+# print(summa)
+
+# multiplication = first_number * second_number
+# print(multiplication)
+
+# division = first_number / second_number
+# print(division)
+
+# rounding
+# print(round(multiplication, 2))
+# print(round(2.5, 0))
+# print(round(3.5, 0))
+# print(round(-3.5, 0))
+# print(int(-3.9))
+# print(abs(-3.555))
+
+# num_to_string = str(multiplication)
+# print(type(num_to_string))
+# print(type(multiplication))
+# print(type(5656565656))
+
+milk_price = 40.35
+milk_quantity = 0.125
+#purchase = Decimal(5)
+# purchase = Decimal(milk_quantity * milk_price)
+#print(purchase)
+PI = math.pi
+print(PI)
+
+mult_may = math.prod([5,5,5,PI])
+print(mult_may)
+
+powering = 5**2
+print(powering)
+
+root =math.sqrt(81)
+print(root)
+
+round_floor = math.floor(-2.9)
+print(round_floor)
+
+round_ceil = math.ceil(-2.9)
+print(round_ceil)
