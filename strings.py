598744aabcf03116c780026404f48b28ae383b37
Vadim Voytyuk <vadimvoytyuk@gmail.com>
2023-05-02 05:34:09 +0300

    slices of strings

--- strings.py
+++ strings.py
@@ -1,16 +1,16 @@
 from hashlib import new
 
-my_love = 'I love python'
+#my_love = 'I love python'
 
 initial_number = 54
 number_one = 5
 number_two = 4
 
-print(id(number_two))
+#print(id(number_two))
 result = number_two * 10 + number_one
 
-print(number_two, number_one, sep='', end='--------')
-print(result)
+#print(number_two, number_one, sep='', end='--------')
+#print(result)
 
 # to many plusses not a good practice
 city = 'Kyiv'
@@ -26,14 +26,14 @@ pass
 # good practice
 # 1 f-string - immediately
 capital_city_correct = f'{city} {country} {continent}'
-print(capital_city_correct)
+#print(capital_city_correct)
 
 # 2-format method
 template = '{city} -  {country} {continent}'
 kyiv_info = template.format(city=city, country=country, continent=continent)
-print(kyiv_info)
+#print(kyiv_info)
 oslo_info = template.format(city=city_o, country=country_n, continent=continent)
-print(oslo_info)
+#print(oslo_info)
 
 # smiles utf -8
 
@@ -43,7 +43,7 @@ fire_1 = 'fire \U0001F525'
 fire_2 = 'fire \t🔥'
 fire_3 = 'fire \N{Fire}'
 fire_4 = r'fire sdvsvsvsv\svsvsv\svsvsv\N{Fire}🔥'  # r (role) - for clasting the outputs in one line
-print(fire_1, fire_2, fire_3, fire_4, end='\n\n\n')
+#print(fire_1, fire_2, fire_3, fire_4, end='\n\n\n')
 
 # user_name = input('please enter your name >>>> ').title() #only title leters
 # user_name = input('please enter your name >>>> ').capitalize()
@@ -59,7 +59,7 @@ print(fire_1, fire_2, fire_3, fire_4, end='\n\n\n')
 
 street = "Baker Street 125 , room 125"
 sherlock_address = street.replace('125', '234', 1)
-print(sherlock_address)
+#print(sherlock_address)
 
 # a->e , e->b (letter by letter)
 # replacer = street.replace('a', 'e')
@@ -68,20 +68,31 @@ print(sherlock_address)
 
 translate_map = str.maketrans('ae', 'eb')
 trans_result = street.translate(translate_map)
-print(trans_result)
+#print(trans_result)
 
 street_length = len(trans_result)
-print(street_length)
+#print(street_length)
 
-from flask import Flask
+#from flask import Flask
 
-app = Flask(__name__)
-
-
-@app.get('/')
-def j():
-    result = sherlock_address + fire_3
-    return result
+#app = Flask(__name__)
 
 
-app.run()
+#@app.get('/')
+#def j():
+   # result = sherlock_address + fire_3
+   # return result
+
+
+#app.run()
+#    0123456789
+my_love = "I love python"
+#                    -2-1
+new_love = my_love[2:5]
+print(new_love)
+
+new_love = my_love[-6:-1]
+new_love = my_love[-6:]
+new_love = my_love[:-5]
+new_love = my_love[::-1]
+print(new_love)
