fb6eef6bc840fe4959068a65a98b3d9e87983a81
Vadim Voytyuk <vadimvoytyuk@gmail.com>
2023-05-08 00:11:09 +0300

    add HW_8_if_else_boolean.py

---
+++ branching.py
@@ -0,0 +1,36 @@
+# user_age = int(input('enter your age (int value)>>>'))
+import password as password
+
+# true = True  # True <> 0 not empty string  comparison result
+# print(bool(user_age))
+# print(bool(0))
+# print(bool(1))
+# print(bool(''))
+# print(bool('user_age'))
+
+# if user_age < 1:  # <> <=.>=.==.!=,is not
+# print('incorrect input')
+# elif user_age == 10:
+# print('You are in 4-th grade')
+# elif user_age >= 18:
+# print('You can by alcohol')
+# else:
+# print('I dont know')
+
+# print(user_age)
+
+# user_name = input('enter your name>>>').title()
+# print(chr(97))
+# print(chr(105))
+# if (user_name == "John") and (password == '1'):
+# print(f"Hello, {user_name} ")
+# else:
+# print('Go away')
+my_string = '55555555555555jjjjjjjjjjjjjjjjj'
+if my_string.isdigit():
+    number = int(my_string)
+    print(number, end='***')
+else:
+    print('Not for int')
+    print(int(my_string.isdigit()))
+"wwwwwwwwwww".
