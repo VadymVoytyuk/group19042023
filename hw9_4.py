1969ad551b90663fc9cd3e14f1aca7e93877a4e4
Vadym Voytyuk <131480043+lunakot@users.noreply.github.com>
2023-05-13 08:24:39 +0300

    Update hw9_4.py

--- hw9_4.py
+++ hw9_4.py
@@ -2,4 +2,3 @@
 celsius_temp = float(input('input temperature reading in celsius degree: '))
 fahrenheit_conversion = ((celsius_temp * 1.8) + 32)
 print(print(str(celsius_temp) + " degree Celsius is equal to " + str(fahrenheit_conversion) + " degree Fahrenheit."))
-gi
